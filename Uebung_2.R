db4f73ee79f812435611ef8d6a6dc5a843702fde
marleenkirchhoff <104210617+marleenkirchhoff@users.noreply.github.com>
2022-05-11 19:41:21 +0200

    Add files via upload

---
+++ Uebung_2.R
@@ -0,0 +1,54 @@
+# sequence alignment, prozentuale Uebereinstimmung
+
+
+setwd ("C:/Users/Marleen Kirchhoff/Desktop/Uni/5. Semester/Bioinformatik")
+data <- read.table(file = "alignedSequencesCalmodulin.txt")
+
+seq1 <- data [1, ]
+seq2 <- data [2, ]
+
+count = 0
+
+for (i in (1 : nchar(seq1)))
+  {if (substr(seq1, i, i) == substr(seq2, i, i)) 
+    {count = count + 1}
+  }
+ 
+
+count/ nchar(seq1)*100
+count 
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              
+              
+
+
+
+
